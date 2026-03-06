5fa06128a61d923d8a664a6566dcd7c757a9f575
nethmif <nethmi.20220716@iit.ac.lk>
2026-03-06 13:00:15 +0530

    Update in processor.py

--- backend/processor.py
+++ backend/processor.py
@@ -22,32 +22,53 @@ import numpy as np
 from torchvision import transforms
 from PIL import Image
 
-def process_ecg_signal(img, target_size=(224, 224)):
-    """
-    Crop, grayscale, binarize, and resize ECG images.
-    Matches preprocessing used during training.
-    """
-    if img is None:
-        return None
+# def process_ecg_signal(img, target_size=(224, 224)):
+#     """
+#     Crop, grayscale, binarize, and resize ECG images.
+#     Matches preprocessing used during training.
+#     """
+#     if img is None:
+#         return None
     
+#     h, w = img.shape[:2]
+#     y_start = int(h * 0.20)
+#     y_end = int(h * 0.95)
+#     if y_end <= y_start:
+#         y_end = h  
+
+#     cropped_img = img[y_start:y_end, :]
+#     gray = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+#     _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+#     kernel = np.ones((2, 2), np.uint8)
+#     clean = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
+#     dilated = cv2.dilate(clean, kernel, iterations=1)
+
+#     resized = cv2.resize(dilated, target_size, interpolation=cv2.INTER_AREA)
+
+#     if len(resized.shape) == 2:
+#         resized = cv2.cvtColor(resized, cv2.COLOR_GRAY2RGB)
+
+#     return resized
+def process_ecg_signal(img, target_size=(224,224)):
+
     h, w = img.shape[:2]
+
     y_start = int(h * 0.20)
     y_end = int(h * 0.95)
-    if y_end <= y_start:
-        y_end = h  
 
-    cropped_img = img[y_start:y_end, :]
-    gray = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
-    _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    cropped = img[y_start:y_end, :]
 
-    kernel = np.ones((2, 2), np.uint8)
-    clean = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
-    dilated = cv2.dilate(clean, kernel, iterations=1)
+    gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
 
-    resized = cv2.resize(dilated, target_size, interpolation=cv2.INTER_AREA)
+    _, binary = cv2.threshold(
+        gray, 0, 255,
+        cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+    )
 
-    if len(resized.shape) == 2:
-        resized = cv2.cvtColor(resized, cv2.COLOR_GRAY2RGB)
+    resized = cv2.resize(binary, target_size)
+
+    resized = cv2.cvtColor(resized, cv2.COLOR_GRAY2RGB)
 
     return resized
 
